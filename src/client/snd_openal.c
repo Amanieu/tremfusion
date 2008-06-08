2392a7ae4ee3cb6fac220eed96614d48baff8bb9
amanieu <amanieu@amanieu-laptop>
2008-06-09 00:32:33 +0800

    * Fixed a compile warning

--- src/client/snd_openal.c
+++ src/client/snd_openal.c
@@ -529,13 +529,13 @@ S_AL_SanitiseVector
 =================
 */
 #define S_AL_SanitiseVector(v) _S_AL_SanitiseVector(v,__LINE__)
-static void _S_AL_SanitiseVector( vec3_t v, int line )
+static void _S_AL_SanitiseVector( const vec3_t v, int line )
 {
 	if( Q_isnan( v[ 0 ] ) || Q_isnan( v[ 1 ] ) || Q_isnan( v[ 2 ] ) )
 	{
 		Com_DPrintf( S_COLOR_YELLOW "WARNING: vector with one or more NaN components "
 				"being passed to OpenAL at %s:%d -- zeroing\n", __FILE__, line );
-		VectorClear( v );
+		VectorClear( (vec_t *)v );
 	}
 }
 
