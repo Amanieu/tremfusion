4ca409281b78ff412cd78089a5cbf5b344a40528
john <john@nhojjohn.isa-geek.com>
2008-08-08 17:20:48 -0400

    $ Changed q3 bot grism to Capt. Pain!

--- botfiles/bots/capt_pain_t.c
+++ botfiles/bots/capt_pain_t.c
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
-chat "grism"
+chat "capt_pain"
 {
 	#include "teamplay.h"
 
