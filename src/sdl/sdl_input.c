649efbe7d206a78a5dc97af7204fc8c7ab022acb
amanieu <devnull@localhost>
2008-08-10 00:37:01 +0800

    * Merge ioq3 svn 1442

--- src/sdl/sdl_input.c
+++ src/sdl/sdl_input.c
@@ -325,7 +325,15 @@ IN_DeactivateMouse
 */
 void IN_DeactivateMouse( void )
 {
-	if (!mouseAvailable || !SDL_WasInit( SDL_INIT_VIDEO ) )
+	if( !SDL_WasInit( SDL_INIT_VIDEO ) )
+		return;
+
+	// Always show the cursor when the mouse is disabled,
+	// but not when fullscreen
+	if( !r_fullscreen->integer )
+		SDL_ShowCursor( 1 );
+
+	if( !mouseAvailable )
 		return;
 
 #ifdef MACOS_X_ACCELERATION_HACK
@@ -351,7 +359,6 @@ void IN_DeactivateMouse( void )
 	{
 		SDL_WM_GrabInput( SDL_GRAB_OFF );
 		SDL_WarpMouse( glConfig.vidWidth / 2, glConfig.vidHeight / 2 );
-		SDL_ShowCursor( 1 );
 
 		mouseActive = qfalse;
 	}
@@ -400,10 +407,10 @@ struct
 
 /*
 ===============
-IN_StartupJoystick
+IN_InitJoystick
 ===============
 */
-static void IN_StartupJoystick( void )
+static void IN_InitJoystick( void )
 {
 	int i = 0;
 	int total = 0;
@@ -454,8 +461,22 @@ static void IN_StartupJoystick( void )
 	Com_DPrintf( "Balls: %d\n", SDL_JoystickNumBalls(stick) );
 
 	SDL_JoystickEventState(SDL_QUERY);
+}
 
-	return;
+/*
+===============
+IN_ShutdownJoystick
+===============
+*/
+static void IN_ShutdownJoystick( void )
+{
+	if (stick)
+	{
+		SDL_JoystickClose(stick);
+		stick = NULL;
+	}
+
+	SDL_QuitSubSystem(SDL_INIT_JOYSTICK);
 }
 
 /*
@@ -789,18 +810,22 @@ void IN_Init(void)
 	in_disablemacosxmouseaccel = Cvar_Get ("in_disablemacosxmouseaccel", "1", CVAR_ARCHIVE);
 #endif
 
-	Cvar_Set( "cl_platformSensitivity", "1.0" );
-
 	SDL_EnableUNICODE(1);
 	SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
 	keyRepeatEnabled = qtrue;
 
-	if (in_mouse->value)
+	if( in_mouse->value )
+	{
 		mouseAvailable = qtrue;
+		IN_ActivateMouse( );
+	}
 	else
+	{
+		IN_DeactivateMouse( );
 		mouseAvailable = qfalse;
+	}
 
-	IN_StartupJoystick( );
+	IN_InitJoystick( );
 	Com_DPrintf ("------------------------------------\n");
 }
 
@@ -809,17 +834,21 @@ void IN_Init(void)
 IN_Shutdown
 ===============
 */
-void IN_Shutdown(void)
+void IN_Shutdown( void )
 {
-	IN_DeactivateMouse();
-
+	IN_DeactivateMouse( );
 	mouseAvailable = qfalse;
 
-	if (stick)
-	{
-		SDL_JoystickClose(stick);
-		stick = NULL;
-	}
+	IN_ShutdownJoystick( );
+}
 
-	SDL_QuitSubSystem(SDL_INIT_JOYSTICK);
+/*
+===============
+IN_Restart
+===============
+*/
+void IN_Restart( void )
+{
+	IN_ShutdownJoystick( );
+	IN_Init( );
 }
