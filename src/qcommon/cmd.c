3d6195794b4c212f4468ac3ca0e861d2da6cc823
amanieu <devnull@localhost>
2008-09-04 21:45:30 +0800

    * Some more fixes

--- src/qcommon/cmd.c
+++ src/qcommon/cmd.c
@@ -1478,7 +1478,7 @@ void Cmd_RemoveCommandVM( const char *cmd_name )
 		return;
 	if( cmd->function )
 	{
-		Com_Error( ERR_DROP, "VM tried to remove system command "
+		Com_Error( ERR_DROP, "Untrusted source tried to remove system command "
 			"\"%s\"\n", cmd_name );
 		return;
 	}
