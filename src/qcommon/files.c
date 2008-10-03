bb4a79b6f7d2a6381a195263cf395cca091ef944
amanieu <amanieu@nogreaterneed.com>
2008-10-03 19:49:56 +0000

    * Don't set fs_basegame to tremfusion by default

--- src/qcommon/files.c
+++ src/qcommon/files.c
@@ -2807,7 +2807,7 @@ static void FS_Startup( const char *gameName )
 
 	fs_debug = Cvar_Get( "fs_debug", "0", 0 );
 	fs_basepath = Cvar_Get ("fs_basepath", Sys_DefaultInstallPath(), CVAR_INIT|CVAR_PROTECTED );
-	fs_basegame = Cvar_Get ("fs_basegame", "tremfusion", CVAR_INIT|CVAR_SYSTEMINFO );
+	fs_basegame = Cvar_Get ("fs_basegame", "", CVAR_INIT|CVAR_SYSTEMINFO );
 	homePath = Sys_DefaultHomePath(&homePath2);
 	if (!homePath || !homePath[0]) {
 		homePath = fs_basepath->string;
