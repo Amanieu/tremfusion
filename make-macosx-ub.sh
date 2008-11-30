e5ce9f9a913f6f5b342d06c22c5a9c89e28a3ba7
amanieu <devnull@localhost>
2008-11-30 16:43:46 +0800

    * Allow make arguments to be passed to make-macosx-ub.sh

--- make-macosx-ub.sh
+++ make-macosx-ub.sh
@@ -124,13 +124,13 @@ if [ -d build/release-darwin-ppc ]; then
 	rm -r build/release-darwin-ppc
 fi
 (ARCH=ppc USE_OPENAL_DLOPEN=1 CC=$PPC_CC CFLAGS=$PPC_CFLAGS \
-	LDFLAGS=$PPC_LDFLAGS make -j$NCPU BUILD_CLIENT_SMP=1) || exit 1;
+	LDFLAGS=$PPC_LDFLAGS make -j$NCPU BUILD_CLIENT_SMP=1 $*) || exit 1;
 
 # intel client and server
 if [ -d build/release-darwin-x86 ]; then
 	rm -r build/release-darwin-x86
 fi
-(ARCH=x86 CFLAGS=$X86_CFLAGS LDFLAGS=$X86_LDFLAGS make -j$NCPU BUILD_CLIENT_SMP=1) || exit 1;
+(ARCH=x86 CFLAGS=$X86_CFLAGS LDFLAGS=$X86_LDFLAGS make -j$NCPU BUILD_CLIENT_SMP=1 $*) || exit 1;
 
 echo "Creating .app bundle $DESTDIR/$APPBUNDLE"
 if [ ! -d $DESTDIR/$APPBUNDLE/Contents/MacOS/$BASEDIR ]; then
