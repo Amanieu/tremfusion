c3d6b87902afe701c1243fd7f613d4c5c16282a3
amanieu <devnull@localhost>
2008-12-18 21:47:52 +0800

    * Fixed the ctrl handler on the console on win32

--- src/sys/sys_win32.c
+++ src/sys/sys_win32.c
@@ -42,6 +42,24 @@ static char homePath[ MAX_OSPATH ] = { 0 };
 static char homePathOld[ MAX_OSPATH ] = { 0 };
 
 /*
+==================
+CON_CtrlHandler
+
+The Windows Console doesn't use signals for terminating the application
+with Ctrl-C, logging off, window closing, etc.  Instead it uses a special
+handler routine.  Fortunately, the values for Ctrl signals don't seem to
+overlap with true signal codes that Windows provides, so calling
+Sys_SigHandler() with those numbers should be safe for generating unique
+shutdown messages.
+==================
+*/
+static BOOL WINAPI CON_CtrlHandler( DWORD sig )
+{
+	Sys_SigHandler( sig );
+	return TRUE;
+}
+
+/*
 ================
 Sys_DefaultHomePath
 ================
@@ -648,9 +666,12 @@ void Sys_PlatformInit( void )
 	}
 	else
 		SDL_VIDEODRIVER_externallySet = qfalse;
+#endif
 
 	// Display a console
 	AllocConsole();
 	SetConsoleTitle("Tremfusion Console");
-#endif
+
+	// Handle Ctrl-C or other console termination
+	SetConsoleCtrlHandler( CON_CtrlHandler, TRUE );
 }
