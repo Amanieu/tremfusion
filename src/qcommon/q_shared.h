4db1b9658fd7a768e75ad564c10fa41442f02516
amanieu <devnull@localhost>
2009-02-08 21:10:43 +0800

    * Fix the color codes to be the same as 1.1

--- src/qcommon/q_shared.h
+++ src/qcommon/q_shared.h
@@ -363,8 +363,7 @@ extern	vec4_t		colorDkGrey;
 
 #define Q_COLOR_ESCAPE	'^'
 #define Q_IsColorString(p)	( p && *(p) == Q_COLOR_ESCAPE && isprint(*((p)+1)) && \
-                              *((p)+1) != Q_COLOR_ESCAPE && *((p)+1) != '.' && \
-                              *((p)+1) != '_' && !isspace(*((p)+1)) )
+                              *((p)+1) != Q_COLOR_ESCAPE && !isspace(*((p)+1)) )
 
 #define COLOR_BLACK		'0'
 #define COLOR_RED		'1'
