2b365d1db3f5248ffb3404d0c995012934647525
amanieu <devnull@localhost>
2009-03-29 01:06:07 +0800

    * Enable SSE, but disable the sse-specific code because it causes crashes

--- Makefile
+++ Makefile
@@ -357,8 +357,6 @@ ifeq ($(PLATFORM),linux)
   else
     ifeq ($(USE_SSE),1)
       BASE_CFLAGS += -msse -mfpmath=sse
-    else
-      BASE_CFLAGS += -U__SSE__ -U__SSE2__
     endif
   endif
 
@@ -443,7 +441,7 @@ ifeq ($(PLATFORM),darwin)
     BASE_CFLAGS += -faltivec
   endif
   ifeq ($(ARCH),x86)
-    OPTIMIZE += -march=prescott -mfpmath=sse -U__SSE__ -U__SSE2__
+    OPTIMIZE += -march=prescott -mfpmath=sse
     # x86 vm will crash without -mstackrealign since MMX instructions will be
     # used no matter what and they corrupt the frame pointer in VM calls
     BASE_CFLAGS += -mstackrealign
--- src/qcommon/q_platform.h
+++ src/qcommon/q_platform.h
@@ -37,10 +37,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #if (defined _M_IX86 || defined __i386__) && !defined(C_ONLY)
 #define id386 1
-#ifdef __SSE2__
+#ifdef USE_SSE2
 #define id386_sse 2
 #else
-#ifdef __SSE__
+#ifdef USE_SSE
 #define id386_sse 1
 #else
 #define id386_sse 0
@@ -49,10 +49,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #else
 #define id386 0
 #if defined __x86_64__ && !defined(C_ONLY)
-#ifdef __SSE2__
+#ifdef USE_SSE2
 #define id386_sse 2
 #else
-#ifdef __SSE__
+#ifdef USE_SSE
 #define id386_sse 1
 #else
 #define id386_sse 0
