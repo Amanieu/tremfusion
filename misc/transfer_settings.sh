dcd2991e5df1c4c27bc30a549d8cb70b436b8b6e
amanieu <devnull@localhost>
2009-03-28 20:15:09 +0800

    * Fixed up transfer_settings.sh

--- misc/transfer_settings.sh
+++ misc/transfer_settings.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Find the paths (different on Macs)
-if [ -d "$HOME/Library/Application Support/Tremulous" ]; then
-	SRCDIR="$HOME/Library/Application Support/Tremulous"
-	DESTDIR="$HOME/Library/Application Support/Tremfusion"
-elif [ -d "$HOME/.tremulous" ]; then
+# Find the path
+if [ -d "$HOME/.tremulous" ]; then
 	SRCDIR="$HOME/.tremulous"
 	DESTDIR="$HOME/.tremfusion"
 else
@@ -51,7 +48,6 @@ echo "Fixing autogen.cfg..."
 sed -e 's/seta\ cl_defaultUI\ \"[^\"]*\"//' \
 	-e 's/seta\ fs_extrapaks\ \"[^\"]*\"//' \
 	-e 's/seta\ s_alDriver\ \"[^\"]*\"//' \
-	-e 's/seta\ s_useOpenAL\ \"[^\"]*\"//' \
 	autogen.cfg > $DESTDIR/autogen.cfg
 
 echo "Done."
