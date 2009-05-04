dbbdc178c3760b3723fea6ad0affeda8c04ad1d8
amanieu <devnull@localhost>
2009-05-04 23:45:19 +0800

    * Removed net_chan backport

--- src/server/sv_net_chan.c
+++ src/server/sv_net_chan.c
@@ -65,12 +65,7 @@ static void SV_Netchan_Encode( client_t *client, msg_t *msg ) {
 		// modify the key with the last received and with this message acknowledged client command
 		if (!string[index])
 			index = 0;
-		if (string[index] > 127 || string[index] == '%') {
-			key ^= '.' << (i & 1);
-		}
-		else {
-			key ^= string[index] << (i & 1);
-		}
+		key ^= string[index] << (i & 1);
 		index++;
 		// encode the data with this key
 		*(msg->data + i) = *(msg->data + i) ^ key;
